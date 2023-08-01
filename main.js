1e4a3f5210635fc96423f38fd7ef7b9b6487003e
Igor Arthur <86696913+iguuh@users.noreply.github.com>
2023-07-31 22:29:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-
 // como pegas as tags/elementos html através dos seletores css
 const screen1 = document.querySelector(".screenOne")
 const screen2 = document.querySelector(".screenTwo")
-
-// geração de um número random
-const randomNumber = Math.round(Math.random() * 10)
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+const randomNumber = Math.round(Math.random() * 10) // geração de um número random
 let xAttemps = 1
+
+// Eventos
+// parte da função callback
+//eventlistener está "ouvindo" o click
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', function(e) {
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleResetClick()
+    }
+})
 
 // Função callback (função passada como argumento de outra função, que será executada mais tarde)
 function handleTryClick (event) {
@@ -15,27 +25,25 @@ function handleTryClick (event) {
     //
     if (Number(inputNumber.value) == randomNumber ) {
         // adição e remoção da lista de classe.
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
+        toggleScreen()
         // adicionar algo dentro de um elemento (screen2)
         screen2.querySelector("h2").innerText = `acertou em ${xAttemps} tentativas`
-    } 
-   
+    }  
     // Pega os valores de um input com value
     inputNumber.value = ""
     xAttemps++
 }
 
-// Eventos
-const btnTry = document.querySelector("#btnTry")
-const btnReset = document.querySelector("#btnReset")
-
-// parte da função callback
-//eventlistener está "ouvindo" o click
-btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function() {
-    screen1.classList.remove("hide")
-        screen2.classList.add("hide")
-        let xAttemps = 1
+function handleResetClick() {
+    toggleScreen()
+    let xAttemps = 1
+    randomNumber = Math.round(Math.random() * 10)
 }
-)
+// faz a troca da screen
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
+
+// alertar sobre numeros maiores que 10 e menores que 0
+// limitar o click
